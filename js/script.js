2460fd05d9b5c922ecaf9ea74bb033ffc2cb9993
anticher <anticherq@gmail.com>
2021-09-18 13:51:05 +0300

    feat: add score to console

---
+++ js/script.js
@@ -0,0 +1,22 @@
+hljs.highlightAll()
+console.log(`
+- layout valid 10/10
+- semantic layout 20/20
+- CSS styles are used for CV design 10/10
+- content is placed in a block that is horizontally centered on the 10/10 page
+- adaptive layout 10/10
+- there is an adaptive burger menu 0/10
+- there is an image on the CV page - a photo or avatar of the author CV 10/10
+- contacts for communication and a list of skills are arranged in the form of a list ul> li 10/10
+- CV contains contacts for communication, brief information about yourself, a list of skills, information about education and level of English 10/10
+- CV contains an example of your code 10/10
+- CV contains image links to projects you completed 0/10
+- CV made in English 10/10
+- Pull Request completed correctly 10/10
+- there is a video of the author in English 0/10
+- design, design, quality of CV performance is not lower than in examples 5/10
+
+- total 125/160
+Thanks for checking and have a nice day =)
+
+`)
